5aecc5e94f03204ff9e7fbea9801e916348eefa7
Jason Bierbrauer <jbierbrauer99@gmail.com>
2017-03-24 19:12:46 -0500

    caesar web application

--- main.py
+++ main.py
@@ -15,33 +15,55 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
 from caesar import *
 
-html = '''<html>
-            <head>
-            <title>Caesar</title>
-            </head>
-            <body>'''
 
-html_end = '''</body>
-            </html>'''
+def buildPage(textarea_content):
+    html = '''<html>
+                <head>
+                <title>Caesar</title>
+                </head>
+                <body bgcolor="lightblue">'''
 
-form = '''<h1>  Web Caesar </h1>
-            <form>
-            <label>Keyword
-            <input type="text" value="keyword">
-            <label>Message
-            <input type="text" value="message">
-            <input type=''' + encrypt'''value="translate">'''
+    html_end = '''</body>
+                </html>'''
 
-text = encrypt('jason is so awesome', 13)
+    form = '''
+            <div align="center">
+            <h1>  Web Caesar </h1>
+                <form method="POST">
+                <label>Message to encrypt
+                <br>
+                <textarea name="message" style="height: 100px; width: 400px;">''' + textarea_content + '''
+                </textarea>
+                <br>
+                <label>Number of times to rotate
+                <br>
+                <input type="number" name="rotation">
+                <br>
+                <input type="submit">
+                </form>
+            </div>'''
 
-main_text = html + text + form + html_end
+    main_text = html + form + html_end
+    return main_text
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write(text)
+        content = buildPage("")
+        self.response.write(content)
+
+    def post(self):
+        message = self.request.get("message")
+        rotation = self.request.get("rotation")
+        encrypted_message = encrypt(message, int(rotation))
+        escaped = cgi.escape(encrypted_message)
+        msg = 'Secret message:' + escaped
+        content = buildPage(msg)
+        self.response.write(content)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
